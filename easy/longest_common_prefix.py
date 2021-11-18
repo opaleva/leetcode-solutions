d52a7716797599184c2661376dd295a95f3d0d93
opaleva <from.aldebaran@gmail.com>
2021-11-18 21:23:26 +0500

    ...

--- easy/longest_common_prefix.py
+++ easy/longest_common_prefix.py
@@ -22,18 +22,46 @@ def scan_horizontally(strings: list[str]) -> str:
 # Vertical scanning (Runtime: 36 ms, Memory Usage: 14.3 MB)
 def scan_vertically(strings: list[str]) -> str:
     if not strings or len(strings) == 0:
-        return ""
+        return "Strings not found"
     for i in range(len(strings[0])):
         c: str = strings[0][i]
         for j in range(1, len(strings)):
             if i == len(strings[j]) or strings[j][i] != c:
                 return strings[0][0:i]
-    return strings[0]
+    # return strings[0]
+
+
+# Divide and conquer (Runtime: 52 ms, Memory Usage: 14.3 MB)
+def get_longest(strings: list[str]) -> str:
+    if not strings or len(strings) == 0:
+        return "Strings not found"
+    return divide(strings, 0, len(strings) - 1)
+
+
+def divide(strings: list[str], left: int, right: int) -> str:
+    if left == right:
+        return strings[left]
+    if right > left:
+        middle: int = left + (right - left) // 2
+        str1: str = divide(strings, left, middle)
+        str2: str = divide(strings, middle + 1, right)
+        return conquer(str1, str2)
+
+
+def conquer(left_lcp, right_lcp):
+    result: str = ""
+    i, j = 0, 0
+    while i <= len(left_lcp) - 1 and j <= len(right_lcp) - 1:
+        if left_lcp[i] != right_lcp[j]:
+            break
+        result += left_lcp[i]
+        i, j = i + 1, j + 1
+    return result
 
 
 def main():
-    strings = ["flower", "flow", "flight"]
-    print(scan_vertically(strings))
+    strings: list[str] = ["flower", "flow", "flight"]
+    print(get_longest(strings))
 
 
 if __name__ == '__main__':
